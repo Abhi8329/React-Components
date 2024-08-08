1bf55c8f25d9807ace272540ad82a60f898c526d
Abhishek Sunil Uphade <113494940+Abhi8329@users.noreply.github.com>
2024-08-08 14:46:20 +0530

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function List() {
+  return (
+    <ul>
+      <li>Bhel</li>
+      <li>Samosa</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+export default List;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//Different Components in React
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
